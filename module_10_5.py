22be42e363e608969c7337defe7199624f6a62ec
Sergey9030 <709030@mail.ru>
2024-09-08 17:55:26 +0300

    Update module_10_5.py

--- module_10_5.py
+++ module_10_5.py
@@ -1,7 +1,6 @@
 import multiprocessing
 import os
 from datetime import datetime
-import time
 
 
 def read_info(name):
@@ -12,19 +11,20 @@ def read_info(name):
             all_data.append(s)
             s = file.readline()
 
-directory = "./Files"
-files = os.listdir(directory)  # Читаем список файлов
-files = [directory + '/' + f for f in files]  #Добавляем к имени файла путь
 
-"""
-# Линейный вызов
-start = datetime.now()
-for f in files:
-    read_info(f)
-print(f'{datetime.now()-start} (линейный)')
-"""
-# Многопрцессорный вызов
 if __name__ == '__main__':
+
+    directory = "./Files"
+    files = os.listdir(directory)  # Читаем список файлов
+    files = [directory + '/' + f for f in files]  #Добавляем к имени файла путь
+
+    # Линейный вызов
+    start = datetime.now()
+    for f in files:
+        read_info(f)
+    print(f'{datetime.now()-start} (линейный)')
+
+    # Многопрцессорный вызов
     start = datetime.now()
     with multiprocessing.Pool(processes=4) as pool:
         pool.map(read_info, files)
